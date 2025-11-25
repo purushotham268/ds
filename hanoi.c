df494fa022d94195d48881926d071170fde14ce2
purushotham268 <purushotham.sachith@gmail.com>
2025-11-25 15:05:44 +0530

    Update and rename towerofhanoi.c to hanoi.c

--- hanoi.c
+++ hanoi.c
@@ -1,17 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 void towerofhanoi(int,char,char,char);
-
-void main(){
+void main()
+{
     int num;
     printf("Enter the number of disks:\n");
     scanf("%d",&num);
     printf("The sequence of moves in tower of hanoi are:\n");
     towerofhanoi(num,'A','B','C');
 }
-
-void towerofhanoi(int n, char from, char to, char aux){
-    if(n==1){
+void towerofhanoi(int n, char from, char to, char aux)
+{
+    if(n==1)
+    {
         printf("\nMove disk 1 from peg %c to peg %c",from,to);
         return;
     }
