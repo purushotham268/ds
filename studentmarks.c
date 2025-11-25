e997a1b1162b3ea4bf1e7a7884d8063ec7814acf
purushotham268 <purushotham.sachith@gmail.com>
2025-11-25 14:52:48 +0530

    Fix formatting and structure in studentmarks.c

--- studentmarks.c
+++ studentmarks.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
-struct student1{
+struct student1
+{
     char name1[20];
     int sub1[10];
 };
-void main(){
+void main()
+{
     struct student1 sem1;
     int n;
     printf("Enter the name of the student: \n");
@@ -11,11 +13,14 @@ void main(){
     printf("Enter the number of the subjects in 1st semester: \n");
     scanf("%d",&n);
     printf("Enter the marks: \n");
-    for(int i = 0;i<n;i++){
+    for(int i = 0;i<n;i++)
+    {
         scanf("%d",&sem1.sub1[i]);
-        }
+    }
     printf("The marks are: \n");
-    for(int i = 0;i<n;i++){
+    for(int i = 0;i<n;i++)
+    {
         printf("%d \n",sem1.sub1[i]);
     }
+
 }
