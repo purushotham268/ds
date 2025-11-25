897fd4e01a4a2afb9c546f197b67b576c7aaaf5e
purushotham268 <purushotham.sachith@gmail.com>
2025-11-25 15:03:40 +0530

    Update stack.c

--- stack.c
+++ stack.c
@@ -6,59 +6,69 @@ void pop();
 void display();
 int stack[SIZE],top = -1;
 
-void main(){
+void main()
+{
     int choice,value;
-    while(1){
+    while(1)
+    {
         printf("Enter your choice:\n");
         printf("Your choices are: \n1.Push, 2.Pop, 3.display, 4.exit\n");
         scanf("%d",&choice);
-        switch(choice){
+        switch(choice)
+        {
             case 1: printf("Enter the value to be inserted:\n");
                     scanf("%d",&value);
                     push(value);
                     break;
-
             case 2: pop();
                     break;
-
             case 3: display();
                     break;
-        
             case 4: exit(0);
                     break;
-        
             default: printf("Invalid input! Please try again!\n");
                     break;                       
         }
     }
 }  
 
-void push(int value){
-    if(top==SIZE-1){
+void push(int value)
+{
+    if(top==SIZE-1)
+    {
         printf("Stack overflow!\n");
     }
-    else{
+    else
+    {
         top++;
         stack[top] = value;
     }
 }
 
-void pop(){
-    if(top==0){
+void pop()
+{
+    if(top==0)
+    {
         printf("Stack underflow!\n");
     }
-    else{
+    else
+    {
         top--;
     }
 }
 
-void display(){
-    if(top==-1){
+void display()
+{
+    if(top==-1)
+    {
         printf("Empty Stack!\n");
     }
-    else{
-        for(int i = 0;i<=top;i++){
+    else
+    {
+        for(int i = 0;i<=top;i++)
+        {
             printf("%d\n",stack[i]);
         }
     }
+
 }
