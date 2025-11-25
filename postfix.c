ca5da20132ada50a1bfafa2557781a8f54161cf7
purushotham268 <purushotham.sachith@gmail.com>
2025-11-25 14:54:21 +0530

    Update postfix.c

--- postfix.c
+++ postfix.c
@@ -4,8 +4,10 @@
 #include <ctype.h>
 
 double compute(char symbol, double p1, double op2);
-double compute(char symbol, double op1, double op2){
-    switch(symbol){
+double compute(char symbol, double op1, double op2)
+{
+    switch(symbol)
+    {
         case '+': return op1+op2;
         break;
         case '-': return op1-op2;
@@ -19,19 +21,23 @@ double compute(char symbol, double op1, double op2){
     }
 }
 
-void main(){
+void main()
+{
     double s[20], res, op1, op2;
     int top=-1, i;
     char postfix[20], symbol;
     printf("Enter a valid postfix expression:\n");
     scanf("%s",postfix);
 
-    for(i=0;i<strlen(postfix);i++){
+    for(i=0;i<strlen(postfix);i++)
+    {
         symbol = postfix[i];
-        if(isdigit(symbol)){
+        if(isdigit(symbol))
+        {
             s[++top] = symbol - '0';
         }
-        else{
+        else
+        {
             op2 = s[top--];
             op1 = s[top--];
             res = compute(symbol, op1 , op2);
@@ -40,4 +46,5 @@ void main(){
     }
     res = s[top--];
     printf("\n The postfix evaluation result: %f",res);
+
 }
