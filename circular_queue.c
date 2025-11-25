fb23c4b128e3797b0fb7fe433c96b6840743f286
purushotham268 <purushotham.sachith@gmail.com>
2025-11-25 14:51:48 +0530

    Fix formatting and improve readability of circular queue

--- circular_queue.c
+++ circular_queue.c
@@ -6,26 +6,25 @@ void enqueue(int);
 void dequeue();
 void display();
 
-int main(){
+int main()
+{
     int value,choice;
-    while(1){
+    while(1)
+    {
         printf("Enter your choice:\n");
         printf("1. Enqueue, 2. Dequeue, 3. Display, 4. Exit\n");
         scanf("%d",&choice);
-        switch(choice){
+        switch(choice)
+        {
             case 1: printf("Enter the value:\n");
                     scanf("%d",&value);
                     enqueue(value);
                     break;
-            
             case 2: dequeue();
                     break;
-
             case 3: display();
                     break;
-
             case 4: exit(0);
-            
             default:printf("Enter a valid choice!\n");
                     break;
         }
@@ -33,11 +32,13 @@ int main(){
 }
 
 void enqueue(int value){
-    if(front == rear + 1 || front == 0 && rear == SIZE - 1){
+    if(front == rear + 1 || front == 0 && rear == SIZE - 1)
+    {
         printf("Queue is full!\n");
     }
     else{
-        if(front==-1){
+        if(front==-1)
+        {
             front = 0;
         }
         rear = (rear+1)%SIZE;
@@ -47,28 +48,34 @@ void enqueue(int value){
 }
 
 void dequeue(){
-    if(front == -1){
+    if(front == -1)
+    {
         printf("Queue is empty!");
     }
-    else{
+    else
+    {
         int element;
         element = cirqueue[front];
-        if(front == rear){
+        if(front == rear)
+        {
             front = -1;
             rear = -1;
         }
-        else{
+        else
+        {
             front = (front+1)%SIZE;
         }
         printf("%d has been deleted!\n",element);
     }
 }
 
-void display(){
+void display()
+{
     printf("The elements are:\n");
     printf("front is %d\n",front);
     for(int i = front; i!=rear; i = (i+1)%SIZE)
         printf("%d\t",cirqueue[i]);
     printf("\n");
     printf("rear is %d\n",rear);
+
 }
